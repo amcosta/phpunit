5b39844c9dd026951569f6c95d70e9bca42b37f0
Alex Moreno <alex.moreno.costa@gmail.com>
2018-08-16 10:21:07 -0300

    Inseri um teste de output

---
+++ tests/Manual/OutputTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Manual;
+
+
+use PHPUnit\Framework\TestCase;
+
+class OutputTest extends TestCase
+{
+    public function testExpectFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
+
+    public function testExpectBarActualBaz()
+    {
+        $this->expectOutputString('bar');
+        print 'baz';
+    }
+}
